05d586ef505c243059d635e18a5bf0739ba3b598
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2024-12-31 00:41:59 +0100

    14/12/2024

--- 2024/14/14.rb
+++ 2024/14/14.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "../../util/board"
+
 Robot = Struct.new(:x, :y, :dx, :dy) do
   def tick(width, height)
     self.x = (x + dx) % width
@@ -10,7 +12,8 @@ end
 initial_robots = File.read("14.txt")
   .lines
   .map { |line| Robot.new(*line.scan(/-?\d+/).map(&:to_i)) }
-robots = initial_robots.dup
+  .freeze
+robots = initial_robots.map(&:clone)
 
 width = 101 # 11
 height = 103 # 7
@@ -28,3 +31,26 @@ c = robots.count { |robot| robot.x > mid_x && robot.y < mid_y }
 d = robots.count { |robot| robot.x > mid_x && robot.y > mid_y }
 
 puts a * b * c * d
+
+robots = initial_robots.map(&:clone)
+found = nil
+
+(1..).each do |i|
+  robots.each { |robot| robot.tick(width, height) }
+
+  board = Board.new(width, height) { 0 }
+  robots.each { |robot| board[robot.x, robot.y] += 1 }
+
+  board.each do |x, y|
+    deltas = [[0, 0], [1, 0], [2, 0], [0, 1], [1, 1], [2, 1], [0, 2], [1, 2], [2, 2]]
+
+    if deltas.all? { |dx, dy| board.in_bounds?(x + dx, y + dy) && board[x + dx, y + dy] == 1 }
+      found = i
+      break
+    end
+  end
+
+  break if found
+end
+
+puts found
